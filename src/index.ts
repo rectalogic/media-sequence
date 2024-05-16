a3c29ed89724f692ecf9de687c37e2b7d6a704e1
Andrew Wason <rectalogic@rectalogic.com>
2024-05-16 12:58:10 -0400

    Initial import from scaffold generator

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { MediaSequence } from './MediaSequence.js';
---
+++ src/media-sequence.ts
@@ -0,0 +1,3 @@
+import { MediaSequence } from './MediaSequence.js';
+
+window.customElements.define('media-sequence', MediaSequence);
