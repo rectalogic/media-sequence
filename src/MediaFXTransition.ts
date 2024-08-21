c761f278f6046af1e473c389d6a2c22d774403ee
Andrew Wason <rectalogic@rectalogic.com>
2024-08-21 11:06:56 -0400

    support multiple transition animations

--- src/MediaFXTransition.ts
+++ src/MediaFXTransition.ts
@@ -4,16 +4,15 @@
 // XXX need <mediafx-json> child and optional <mediafx-style> which is just the styles with no selector, and use ::slotted(.source)/::slotted(.dest) as selectors
 // XXX hmm, use <mediafx-target type="source">/<mediafx-target type="dest"> to contain the above
 // XXX need to support canned transitions too
-//XXX make <mediafx-json> support src attribute and load the json - use in Effect too
 
 import { fromError } from 'zod-validation-error';
-import { effectSchema, EffectInfo } from './schema/Effect.js';
+import { effectSchema } from './schema/Effect.js';
 import MediaFXContent from './MediaFXContent.js';
 
 type TargetType = 'source' | 'dest';
 type TransitionInfo = {
   style?: CSSStyleSheet;
-  animation?: Animation;
+  animations?: Animation[];
 };
 
 const template = document.createElement('template');
@@ -45,27 +44,32 @@ export class MediaFXTransition extends HTMLElement {
         `:scope > mediafx-target[type="${targetType}"`,
       );
       if (target) {
-        const targetJson = await target
-          .querySelector<MediaFXContent>(
-            ':scope > mediafx-content[type="application/json"]',
-          )
-          ?.content();
         const targetStyle = await target
           .querySelector<MediaFXContent>(
             ':scope > mediafx-content[type="text/css"]',
           )
           ?.content();
+        const targetJson = await Promise.all(
+          Array.from(
+            target.querySelectorAll<MediaFXContent>(
+              ':scope > mediafx-content[type="application/json"]',
+            ),
+          ).map(async tj => tj.content()),
+        );
         const result: TransitionInfo = {};
-        if (targetJson) {
-          const effect = effectSchema.parse(targetJson);
-          const options = {
-            ...effect.options,
-            duration: this.duration / (effect.options?.iterations || 1),
-          };
-          result.animation = new Animation(
-            new KeyframeEffect(targetElement, effect.keyframes, options),
-          );
-          result.animation.pause();
+        if (targetJson.length > 0) {
+          result.animations = targetJson.map(tj => {
+            const effect = effectSchema.parse(tj);
+            const options = {
+              ...effect.options,
+              duration: this.duration / (effect.options?.iterations || 1),
+            };
+            const animation = new Animation(
+              new KeyframeEffect(targetElement, effect.keyframes, options),
+            );
+            animation.pause();
+            return animation;
+          });
         }
         if (targetStyle) {
           const sheet = new CSSStyleSheet();
